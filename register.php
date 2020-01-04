cc7d1723d07b9f9ec7a844fb6c83d834614f2167
shin <5446shin@gmail.com>
2020-01-04 15:58:34 +0900

    First commit and made base login form

---
+++ register.php
@@ -0,0 +1,42 @@
+<?php
+    if (isset($_POST["submitButton"])) {
+        echo "Form was submitted";
+    }
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Welcome to Shinflix</title>
+        <link rel="stylesheet" type="text/css" href="assets/style/style.css" />
+    </head>
+    <body>
+
+        <div class="signInContainer">
+
+            <div class="column">
+
+                <form method="POST">
+
+                    <input type="text" name="firstName" placeholder="First name" required>
+
+                    <input type="text" name="lastName" placeholder="Last name" required>
+
+                    <input type="text" name="userName" placeholder="Username" required>
+
+                    <input type="email" name="email" placeholder="Email" required>
+
+                    <input type="email" name="email2" placeholder="Confirm email" required>
+
+                    <input type="password" name="password" placeholder="Password" required>
+
+                    <input type="password" name="password2" placeholder="Confirm Password" required>
+
+                    <input type="submit" name="submitButton" value="SUBMIT">
+
+                </form>
+            </div>
+
+        </div>
+
+    </body>
+</html>
