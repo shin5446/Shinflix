7409da48381b5b94b78ebd228b8ab3fa4edb9249
shin <5446shin@gmail.com>
2020-01-05 15:17:01 +0900

    Implement saving account

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -14,6 +14,27 @@ class Account {
         $this->validateUserName($un);
         $this->validateEmails($em, $em2);
         $this->validatePassword($pw, $pw2);
+
+        if(empty($this->errorArray)) {
+            return $this->insertUserDetails($fn, $ln, $un, $em, $pw);
+        }
+
+        return false;
+    }
+
+    private function insertUserDetails($fn, $ln, $un, $em, $pw) {
+
+        $pw = hash("sha512", $pw);
+
+        $query = $this->con->prepare("INSERT INTO users (firstName, lastName, username, email, password)
+                                        VALUES (:fn, :ln, :un, :em, :pw)");
+        $query->bindValue(":fn", $fn);
+        $query->bindValue(":ln", $ln);
+        $query->bindValue(":un", $un);
+        $query->bindValue(":em", $em);
+        $query->bindValue(":pw", $pw);
+
+        return $query->execute();
     }
 
     private function validateFirstName($fn) {
--- register.php
+++ register.php
@@ -16,7 +16,12 @@ $account = new Account($con);
         $password = FormSanitizer::sanitizeFormPassword($_POST["password"]);
         $password2 = FormSanitizer::sanitizeFormPassword($_POST["password2"]);
 
-        $account->register($firstName, $lastName, $username, $email, $email2, $password, $password2);
+        $success = $account->register($firstName, $lastName, $username, $email, $email2, $password, $password2);
+
+        if($success) {
+            // Store session
+            header("Location: index.php");
+        }
     }
 ?>
 <!DOCTYPE html>
